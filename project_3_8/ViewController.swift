f7fd68e9d348fed494d2ed33d104a3fcc2224b1f
Lizn <wwwliuzhencom@vip.qq.com>
2017-03-13 10:19:51 +0800

    calculator
    计算器模版

---
+++ project_3_8/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  project_3_8
+//
+//  Created by 20151104689 on 17/3/8.
+//  Copyright © 2017年 20151104689. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func plus(_ sender: Any) {
+        
+    }
+    @IBAction func minus(_ sender: Any) {
+    
+    }
+    @IBAction func multiply(_ sender: Any) {
+    
+    }
+    @IBAction func divided(_ sender: Any) {
+    
+    }
+    @IBAction func equal(_ sender: Any) {
+    
+    }
+    
+    @IBAction func one(_ sender: Any) {
+    }
+    @IBAction func two(_ sender: Any) {
+    }
+    @IBAction func three(_ sender: Any) {
+    }
+    @IBAction func four(_ sender: Any) {
+    }
+    @IBAction func five(_ sender: Any) {
+    }
+    @IBAction func six(_ sender: Any) {
+    }
+    @IBAction func seven(_ sender: Any) {
+    }
+    @IBAction func eight(_ sender: Any) {
+    }
+    @IBAction func nine(_ sender: Any) {
+    }
+    @IBAction func zero(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
